d1f06413de97d233e7f303858bafb7fc02a046dd
Corey J. Nolet <cjnolet@gmail.com>
2019-06-10 11:02:04 -0400

    Refactoring to be more readable

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ library_dirs = [cuda_lib_dir, cuml_lib, comms_lib]
 if os.environ.get("CONDA_PREFIX", None):
     include_dirs.append(os.environ.get("CONDA_PREFIX") + "/include")
 
-srcs = ["nccl-example/simple_reduce.pyx", "nccl-example/simple_reduce.cpp"]
+srcs = ["nccl-example/simple_reduce.pyx", "nccl-example/simple_reduce_api.cpp"]
 
 extensions = [Extension("nccl_example",
                         sources=srcs,
